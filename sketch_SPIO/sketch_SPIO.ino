9dcf687cbff3a21ee90deb0064c5134d9405986b
jungsu00 <hakimz@naver.com>
2023-11-08 17:14:19 +0900

    SPI

--- sketch_SPIO/sketch_SPIO.ino
+++ sketch_SPIO/sketch_SPIO.ino
@@ -4,47 +4,44 @@
 int buttonPin = 2;
 int ledPin =7;
 volatile boolean received;      // volatile 휘발성 함수
-volatile byte slaveReceived, slaveSend;
+volatile uint8_t slaveReceived, slaveSend;
 int buttonValue;
 int x;
 
 void setup()
 {
   Serial.begin(1000000);
-//  pinMode(ledPin, OUTPUT);
-//  pinMode(buttonPin, INPUT);
+
+  pinMode(MOSI, INPUT);
   pinMode(MISO, OUTPUT);
+  pinMode(SCK, INPUT);
+  pinMode(SS, INPUT);
+//  SPI.setClockDivider(SPI_CLOCK_DIV16);
+
   SPCR |=_BV(SPE);
   SPCR &= ~_BV(MSTR);
   SPCR|=_BV(SPIE);
-  received=false;
+  
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 ISR(SPI_STC_vect){
 
   slaveReceived = SPDR;
   Serial.println(slaveReceived);
-      if (slaveReceived==1) 
-      {
-        digitalWrite(ledPin,HIGH);         
-        Serial.println("Slave LED ON");
-      }else
-      {
-        digitalWrite(ledPin,LOW);          
-        Serial.println("Slave LED OFF");
-      }
-      
-      buttonValue = digitalRead(buttonPin);  
-      // if (buttonValue == LOW)              
-      //   x=1;
-        
-      // }else
-      // {
-      //   x=0;
-      // }
-      
-      // slaveSend=x;                             
-      // SPDR = slaveSend; 
+
+    if (slaveReceived == 1) 
+    {
+      digitalWrite(ledPin,HIGH);         
+      Serial.println("Slave LED ON");
+    }
+    
+    else
+    {
+      digitalWrite(ledPin,LOW);     
+      Serial.println("Slave LED OFF");
+    }
 }
 
 void loop()
